fd8d96f53ec0d0ac36e45f561f639128dc0755c4
meoxjdn <120544758+meoxjdn@users.noreply.github.com>
2026-03-29 14:00:31 +0800

    Update hbp_hook.c

--- hbp_hook.c
+++ hbp_hook.c
@@ -15,7 +15,8 @@ void hbp_handler(struct perf_event *bp, struct perf_sample_data *data, struct pt
 
     // 1. 全屏广角 (FPSIMD 修改)
     if (g_fov_on && pc == base_addr + OFF_FOV) {
-        struct user_fpsimd_state *fpsimd = &current->thread.uw.fpsimd_state;
+        // [修复] 移除 .uw，直接调用标准 GKI 结构
+        struct user_fpsimd_state *fpsimd = &current->thread.fpsimd_state;
         ((__u32 *)&fpsimd->vregs[0])[0] = 0x4089999A; // 锁定 4.3f
         set_tsk_thread_flag(current, TIF_FOREIGN_FPSTATE); // 强制刷入硬件
         regs->pc = regs->regs[30]; // 函数起点直接 RET
@@ -38,13 +39,13 @@ void hbp_handler(struct perf_event *bp, struct perf_sample_data *data, struct pt
     if (g_damage_on && pc == base_addr + OFF_DAMAGE) {
         uint32_t team_id = 0;
         void __user *ptr = (void __user *)(regs->regs[1] + 0x1c);
+        
         if (regs->regs[1] != 0 && access_ok(ptr, 4)) {
-            pagefault_disable();
-            if (__copy_from_user_inatomic(&team_id, ptr, 4) == 0 && team_id == 0) {
+            // [修复] 使用 GKI 推荐的 nofault 读取机制，天生安全，不需要配对 disable
+            if (copy_from_user_nofault(&team_id, ptr, 4) == 0 && team_id == 0) {
                 regs->regs[0] = 1; 
                 regs->pc = regs->regs[30]; // 玩家无敌
             }
-            pagefault_enable();
         }
         goto out;
     }
