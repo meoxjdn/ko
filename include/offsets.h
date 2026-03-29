f48077a08784b7248e525b50dec7e1394ea318c2
meoxjdn <120544758+meoxjdn@users.noreply.github.com>
2026-03-29 13:50:55 +0800

    Update offsets.h

--- include/offsets.h
+++ include/offsets.h
@@ -1,6 +1,11 @@
-#define OFF_FOV      0x9326F78
-#define OFF_BORDER   0x8951160
-#define OFF_SKIP     0x2639fd8
-#define OFF_SKIP_JMP 0x53709a0
-#define OFF_DAMAGE   0x844f4b4
-#define OFF_MAXHP    0x33b2ffc
+#ifndef _OFFSETS_H_
+#define _OFFSETS_H_
+
+#define OFF_FOV      0x9326F78   // 截图末尾 F78
+#define OFF_BORDER   0x8951160   // 截图末尾 160
+#define OFF_SKIP     0x2639fd8   // 截图末尾 FD8
+#define OFF_SKIP_JMP 0x53709a0   // 结算函数入口
+#define OFF_DAMAGE   0x844f4b4   // 截图末尾 4B4
+#define OFF_MAXHP    0x33b2ffc   // 截图末尾 FFC
+
+#endif
